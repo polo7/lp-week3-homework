6b26bc569eecbf58427d61f88845c8501e3bf0f9
Roman Lesechko <polo-ru@yandex.ru>
2020-02-28 01:02:48 +0300

    Доп.задачи - Уровень3 - Города

--- cities/cities-bot.py
+++ cities/cities-bot.py
@@ -21,3 +21,50 @@ with open('ru_cities.txt', 'r', encoding='utf-8') as f_input:
         test_dict[c].append(line.rstrip('\n'))
 
 print(test_dict)
+
+
+# Ссылка на моего бота - http://t.me/LP16UberBot
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO,filename='bot.log')
+
+PROXY = {
+    'proxy_url': 'socks5://t3.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn', 
+        'password': 'python'
+    }
+}
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def play_cities(bot, update):
+    pass
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+ 
+
+def main():
+    mybot = Updater("1036004253:AAH6OgKQU0xZH7kDUtURSoaJ2Uvpjvvg8Jc", request_kwargs=PROXY)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("play", play_cities))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
+    mybot.start_polling()
+    mybot.idle()
+       
+
+if __name__ == "__main__":
+    main()
